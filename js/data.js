f0c9cea3e0e29824a6a288428fd01f7dfa0d5475
Aman Kumar Dewangan <amandewatnitrr@gmail.com>
2021-10-04 13:53:07 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -12,7 +12,61 @@ const workshopData = [
     image:
       "https://media.istockphoto.com/photos/businesswoman-holding-a-speech-picture-id1254127323?b=1&k=20&m=1254127323&s=170667a&w=0&h=l7PIoqgPnTZeA5fINQ22ShbZt9gOxaCDbxXu0yxf5fQ=", //give the image (make sure to host it before adding)
     date: "5th Oct 2021", //give the event's date
-    time: "5 PM Onwards", //give the event's time
+    time: "5 PM IST Onwards", //give the event's time
+  },
+  {
+    workshopName: "Knock’em Out: Solving DSA and CP",
+    about: "Getting started with DSA and CP for cracking Coding Round and Interviews..", //About the workshop in about 50 words
+    link: "https://discord.gg/WRNevv9vWf", //enter the link of the workshop
+    image:
+      "https://github.com/amandewatnitrr/Octafest/blob/main/img/posters/sanjeeb%20.-0.jpg?raw=true", //give the image (make sure to host it before adding)
+    date: "6th Oct 2021", //give the event's date
+    time: "5 PM IST Onwards", //give the event's time
+  },
+  {
+    workshopName: "Open Source Contribution with Mathematica/Wolfram",
+    about: "Getting started with Wolfram and how you can do Open-Source with Wolfram", //About the workshop in about 50 words
+    link: "https://discord.gg/WRNevv9vWf", //enter the link of the workshop
+    image:
+      "https://github.com/amandewatnitrr/Octafest/blob/main/img/posters/aman%20sir0010.jpg?raw=true", //give the image (make sure to host it before adding)
+    date: "8th Oct 2021", //give the event's date
+    time: "5 PM IST Onwards", //give the event's time
+  },
+  {
+    workshopName: "Valorant Tournament",
+    about: "All or Nothing", //About the workshop in about 50 words
+    link: "https://discord.gg/WRNevv9vWf", //enter the link of the workshop
+    image:
+      "https://github.com/amandewatnitrr/Octafest/blob/main/img/posters/Customs!%20(3).png?raw=true", //give the image (make sure to host it before adding)
+    date: "9th Oct 2021", //give the event's date
+    time: "7:30 PM IST Onwards", //give the event's time
+  },
+  {
+    workshopName: "Latex",
+    about: "Learning Documentation with Latex", //About the workshop in about 50 words
+    link: "https://discord.gg/WRNevv9vWf", //enter the link of the workshop
+    image:
+      "https://github.com/amandewatnitrr/Octafest/blob/main/img/posters/ankitaa0.jpg?raw=true", //give the image (make sure to host it before adding)
+    date: "10th Oct 2021", //give the event's date
+    time: "5 PM IST Onwards", //give the event's time
+  },
+  {
+    workshopName: "Let’s flutter together",
+    about: "Getting Started with Flutter", //About the workshop in about 50 words
+    link: "https://discord.gg/WRNevv9vWf", //enter the link of the workshop
+    image:
+      "https://github.com/amandewatnitrr/Octafest/blob/main/img/posters/virenn0.jpg?raw=true", //give the image (make sure to host it before adding)
+    date: "12th Oct 2021", //give the event's date
+    time: "5 PM IST Onwards", //give the event's time
+  },
+  {
+    workshopName: "Postman API 101",
+    about: "Getting started with API Testing", //About the workshop in about 50 words
+    link: "https://discord.gg/WRNevv9vWf", //enter the link of the workshop
+    image:
+      "https://github.com/amandewatnitrr/Octafest/blob/main/img/posters/anchal0.jpg?raw=true", //give the image (make sure to host it before adding)
+    date: "16th Oct 2021", //give the event's date
+    time: "5 PM IST Onwards", //give the event's time
   },
 ];
 
